32e194021d630860effa77fbba5807014ce93039
Alex Meijer <asure007@gmail.com>
2025-05-24 01:03:56 +0200

    Adjust .lod to 0x1000000 offset, add rom generator
    Regenerate tbl and asm files to new offset!
    Run makeimgr.cmd to output roms.
    We have 4Mbyte to burn here, only using 0.7 atm.
    (Should fit into 4x27C020 chips for testing on real hardware.)

---
+++ BACKUP/XCODE101L/IMG/makeimgr.bat
@@ -0,0 +1,12 @@
+rem 0x1000000-0x1400000 (loadr misc3.bin,1000000,0,:gfxrom in mame debug!)
+md rom
+del rom\*.u*
+c:\bin\far\srec_cat misc3.bin -binary -split 4 0 -o rom\l1.0_nba_hangtime_u_117_image_rom.u117 -binary
+c:\bin\far\srec_cat misc3.bin -binary -split 4 1 -o rom\l1.0_nba_hangtime_u_116_image_rom.u116 -binary
+c:\bin\far\srec_cat misc3.bin -binary -split 4 2 -o rom\l1.0_nba_hangtime_u_115_image_rom.u115 -binary
+c:\bin\far\srec_cat misc3.bin -binary -split 4 3 -o rom\l1.0_nba_hangtime_u_114_image_rom.u114 -binary
+rem extend files to 1Mbyte
+qfsutil file seteof rom\l1.0_nba_hangtime_u_117_image_rom.u117 1048576
+qfsutil file seteof rom\l1.0_nba_hangtime_u_116_image_rom.u116 1048576
+qfsutil file seteof rom\l1.0_nba_hangtime_u_115_image_rom.u115 1048576
+qfsutil file seteof rom\l1.0_nba_hangtime_u_114_image_rom.u114 1048576
