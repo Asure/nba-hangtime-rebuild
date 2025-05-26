0584ef13ab25e126cec515feb4d0d005b88ae89e
Alex Meijer <asure007@gmail.com>
2025-05-26 14:39:25 +0200

    Update img generation
    - BBVDA in IROM and Unzip didn't play nice.
    - VDADEBUG in SYS.EQU for testing.
    - DosBox moar speed attempts.

---
+++ BACKUP/XCODE101L/IMG/vdastuff/IMGT.cmd
@@ -0,0 +1,4 @@
+@echo off
+rem this CMD file will do everything and create bb3.bin.
+rem adjust paths as needed.
+c:\dosbox-x\dosbox-x.exe -fastlaunch -conf ..\dosbox-x\dosbox-x.conf exit -c "c:\myht\backup\xcode1~3\img\vdastuff\imgt.bat"
