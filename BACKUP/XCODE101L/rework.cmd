789dcc8f33942f85519cc7e3c34f7b0ae6940f02
Alex Meijer <asure007@gmail.com>
2025-07-01 16:53:14 +0200

    Fatalities 100% implemented
    Ported the entire fatality feature from NBA Jam TE.
    But at a much cheaper way in terms of ROM space.
    
    - Behind: Hold UP + PASS PASS code at matchup/code screen.
    - You must be on Fire
    - Your entire team gets fire push fatals
    - It's 40c here hot damn i'm on fire too!

--- BACKUP/XCODE101L/rework.cmd
+++ BACKUP/XCODE101L/rework.cmd
@@ -1,4 +1,4 @@
-"C:\Program Files (x86)\DOSBox-0.74\DOSBox.exe" -fastlaunch -conf .\dosbox-x\dosbox.conf -noconsole
+"C:\Program Files (x86)\DOSBox-0.74\DOSBox.exe" -fastlaunch -conf .\dosbox-x\dosbox-ECE.conf -noconsole
 del mc*.*
 cd ROM
 merge.cmd
