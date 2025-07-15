369cf4b756ca34b75a04e39ff4b7770140015586
Alex Meijer <asure007@gmail.com>
2025-07-16 00:53:25 +0200

    Experimental DCS encode. Some new dunk stuff, initial cowering.

--- BACKUP/XCODE101L/DCS/rebuild.cmd
+++ BACKUP/XCODE101L/DCS/rebuild.cmd
@@ -1,6 +1,6 @@
 rem working with nbamht!
 del nbatest10.zip
-DCSEncoder.exe -o nbatest10.zip --stream-dir=.\input --patch .\ORIGROM\nba.zip rebuild.scr >rebuild.txt
+NEWDCS.exe -o nbatest10.zip --stream-dir=.\input --patch .\ORIGROM\nba.zip rebuild.scr >rebuild.txt
 rem DCSexplorer.exe -p nbatest10.zip >new_prog.txt
 mkdir output
 cd output
