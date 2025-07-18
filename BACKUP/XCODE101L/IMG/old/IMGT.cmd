3021b470ee1ba5665a36d47d355976b5fb366f36
Alex Meijer <asure007@gmail.com>
2025-07-19 00:45:21 +0200

    Fixes and cleanup
    Proper handling of disabled PUTBACK in sys.equ!
    Merge LOD files, update IMGT.CMD and friends.

--- BACKUP/XCODE101L/IMG/old/IMGT.cmd
+++ BACKUP/XCODE101L/IMG/old/IMGT.cmd
@@ -3,11 +3,11 @@ rem this CMD file will do everything and create bin and roms, and zip the whole 
 rem adjust paths as needed.
 rem 29-5 now with auto-trimming the last 35 lines from imgpal8.asm with powershell.
 c:\dosbox-x\dosbox-x.exe -fastlaunch -conf .\dosbox-x\dosbox-x.conf exit -c "c:\myht\backup\xcode1~3\img\imgt.bat"
-del misc.old
-ren misc.bin misc.old
-dd if=misc.irw of=misc.bin bs=0x44 skip=1
-rem dd if=misc3.irw of=misc3.bin bs=0x44 skip=1
-rem dd if=misc4.irw of=misc4.bin bs=0x44 skip=1
+del misc2.old
+ren misc2.bin misc2.old
+dd if=misc2.irw of=misc2.bin bs=0x44 skip=1
+dd if=misc3.irw of=misc3.bin bs=0x44 skip=1
+dd if=misc4.irw of=misc4.bin bs=0x44 skip=1
 rem MAME debug commands to load these from the mame folder:
 rem loadr misc.bin,1200000,0,:gfxrom in mame debug
 copy misc*.bin D:\mame
@@ -17,8 +17,7 @@ start makeimgr
 cd ..
 rem cut the last 70 lines (these are duplicate PALS we dont need.)
 rem linux "head -n -70 input.txt > temp && mv temp input.txt" for example.
-powershell -Command "$i=0; Get-Content imgpal8.asm | Where-Object { ++$i -lt 8 -or $i -gt 44 } | Set-Content imgpal16.asm"
-powershell -Command "Get-Content IMGPAL16.ASM | Select-Object -First ((Get-Content IMGPAL16.ASM).Count - 70) | Set-Content IMGPAL18.ASM"
+powershell -Command "Get-Content IMGPAL8.ASM | Select-Object -First ((Get-Content IMGPAL8.ASM).Count - 70) | Set-Content IMGPAL18.ASM"
 del imgpal8.asm
 ren imgpal18.asm IMGPAL8.ASM
 rem pause
